0bae3675f18938c140a81a94b9ac00f34fc9018d
aavdisd07 <2021bit153@sggs.ac.in>
2024-08-22 15:13:56 +0530

    ui changes

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "./LoginSignup.css";
 
 import user_icon from "../Assets/user.png";
-import email_icon from "../Assets/email.png";
-import password_icon from "../Assets/padlock.png";
+import email_icon from "../Assets/mail.png";
+import password_icon from "../Assets/dialpad.png";
+import org_icon from  "../Assets/organization.png";
 
 const LoginSignup = () => {
   const [action, setAction] = useState("Login");
@@ -11,15 +12,20 @@ const LoginSignup = () => {
   return (
     <div className="container">
       <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
+        
+      <div className="underline"></div>
+      <div className="text">{action}</div>
+        
       </div>
       <div className="inputs">
         {action === "Login" ? <div></div> : (
-          <div className="input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder="Username" />
-          </div>
+           <><div className="input">
+            <img src={org_icon} alt="" />
+            <input type="text" placeholder="Organization name" />
+          </div><div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Admin name" />
+            </div></>
         )}
 
         <div className="input">
@@ -28,15 +34,15 @@ const LoginSignup = () => {
         </div>
         <div className="input">
           <img src={password_icon} alt="" />
-          <input type="password" placeholder="Password" />
+          <input type="tel" placeholder="Mobile Number" />
         </div>
       </div>
 
-      {action === "Sign Up" ? <div></div> : (
+      {/* {action === "Sign Up" ? <div></div> : (
         <div className="forgot-password">
           Forgot password? <span>Click Here!</span>
         </div>
-      )}
+      )} */}
 
       <div className="submit-container">
         <div
